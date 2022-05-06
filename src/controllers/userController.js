11970502909eaf9fb6eaa4f32af233b4fe8dfe61
crystal23733 <48302668+crystal23733@users.noreply.github.com>
2022-05-06 10:12:08 +0900

    Wetube Server

---
+++ src/controllers/userController.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+export const join = (req, res) => res.send("Join");
+export const edit = (req, res) => res.send("Edit User");
+export const remove = (req, res) => res.send("Remove User");
+export const login = (req, res) => res.send("Login");
+export const logout = (req, res) => res.send("Log out");
+export const see = (req, res) => {
+    console.log(req.params);
+    return res.send("See User");
+}
