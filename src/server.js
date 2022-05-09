74741f4aa98f8e8a11ec45898f8ac459ec15e615
crystal23733 <48302668+crystal23733@users.noreply.github.com>
2022-05-09 12:19:46 +0900

    pug

--- src/server.js
+++ src/server.js
@@ -9,6 +9,9 @@ const app = express();
 const PORT = 4000;
 
 const logger = morgan("dev");
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 
 app.use("/", globalRouter);
