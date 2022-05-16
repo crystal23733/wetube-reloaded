3aa79ae77b4b9ad5d9fd33b854cc87134503ba72
crystal23733 <48302668+crystal23733@users.noreply.github.com>
2022-05-16 22:58:07 +0900

    mongoose

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,43 +1,36 @@
-import express from "express";
+import Video from "../models/Video";
 
-let videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 1,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 2,
-    },
-    {
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 3,
-    },
-];
-
-export const trending = (req, res) => {
-    return res.render("home", {pageTitle : "Home", videos});
+// console.log("start");
+// Video.find({}, (error, videos) => {
+//   console.log("Finished");
+//   return res.render("home", {pageTitle : "Home", videos : []});
+// });
+// console.log("I finish first");
+export const home = async(req, res) => {
+  const videos = await Video.find({});
+  return res.render("home", {pageTitle : "Home", videos : []});
 }
 export const watch = (req, res) => {
-    const { id } = req.params;
-    const video = videos[id - 1];
-    res.render("watch", {pageTitle : `Watching ${video.title}`, video})
+  const { id } = req.params;
+  res.render("watch", {pageTitle : `Watching : `})
 };
-export const edit = (req, res) => res.render("edit", {pageTitle : "Edit"});
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  res.render("edit", {pageTitle : `Editing : `})
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  return res.redirect(`/video/${id}`);
+};
+export const getUpload = (req, res) => {
+  return res.render("upload", {pageTitle : "Upload video"});
+}
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  return res.redirect("/");
+}
 export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
     console.log(req.params);
     return res.send("Delete Video");
--- src/server.js
+++ src/server.js
@@ -6,18 +6,14 @@ import videoRouter from "./routers/videoRouter";
 
 const app = express();
 
-const PORT = 4000;
-
 const logger = morgan("dev");
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-
+app.use(express.urlencoded({extended : true}));
 app.use("/", globalRouter);
 app.use("/user", userRouter);
 app.use("/video", videoRouter);
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`);
-
-app.listen(PORT, handleListening);
+export default app;
