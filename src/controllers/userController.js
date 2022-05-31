c3146b3de3b36623df6a6f615d74aca8e0189b1d
crystal23733 <48302668+crystal23733@users.noreply.github.com>
2022-05-31 11:42:18 +0900

    passwordHash

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,19 @@
 import express from "express";
+import User from "../models/User";
 
 export const getJoin = (req, res) => {
     res.render("join", {pagetitle:"join"});
 };
-export const postJoin = (req, res) => {
-    console.log(req.body);
-    res.end();
+export const postJoin = async (req, res) => {
+    const { email, username, password, name, location } = req.body;
+    await User.create({
+        email,
+        username,
+        password,
+        name,
+        location,
+      });
+    return res.redirect("/login");
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
--- src/models/User.js
+++ src/models/User.js
@@ -1,13 +1,19 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
-    email: {type:String, required:true, unique:true},
-    username: {type:String, required:true, unique:true},
-    password: {type:String, required:true},
-    name: {type:String, required:true},
-    location: {type:String},
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+  location: String,
 });
 
-const User = mongoose.model("user", userSchema);
+userSchema.pre("save", async function() {
+    console.log(this.password);
+    this.password = await bcrypt.hash(this.password, 5);
+    console.log(this.password);
+});
 
+const User = mongoose.model("User", userSchema);
 export default User;
