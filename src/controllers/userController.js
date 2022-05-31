22aae8ef0858424592bcf28760ab8c77fc0e2d1a
crystal23733 <48302668+crystal23733@users.noreply.github.com>
2022-06-01 03:58:31 +0900

    Form Validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,7 +5,21 @@ export const getJoin = (req, res) => {
     res.render("join", {pagetitle:"join"});
 };
 export const postJoin = async (req, res) => {
-    const { email, username, password, name, location } = req.body;
+    const pageTitle = "Join";
+    const { email, username, password, password2, name, location } = req.body;
+    const exists = await User.exists({ $or: [{ username }, { email }] });
+    if(password !== password2){
+        return res.render("join", {
+            pageTitle,
+            errorMessage : "비밀번호가 일치하지 않습니다.",
+        });
+    };
+    if(exists){
+        return res.render("join", {
+            pageTitle,
+            errorMessage : "닉네임 또는 이메일이 사용중입니다.",
+        });
+    };
     await User.create({
         email,
         username,
