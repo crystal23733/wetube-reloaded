74200980666c8d2dc2c5ab6bd1a160d62f866bc9
crystal23733 <48302668+crystal23733@users.noreply.github.com>
2022-06-07 09:41:11 +0900

    session

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -53,6 +53,8 @@ export const postLogin = async (req, res) => {
             errorMessage:"비밀번호가 틀립니다.",
         });
     };
+    req.session.loggedIn = true;
+    req.session.user = user;
     return res.redirect("/");
 };
 export const edit = (req, res) => res.send("Edit User");
