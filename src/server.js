649b2ec50d43bdb829b2f4d16e6a6c6fd2be1113
crystal23733 <48302668+crystal23733@users.noreply.github.com>
2023-05-04 00:33:55 +0900

    Better Developer Experience

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ app.use(
 );
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
-app.use("/assets", express.static("assets"));
+app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/video", videoRouter);
